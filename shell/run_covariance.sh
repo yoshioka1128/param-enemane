2a334bf2524064d0bd2d71b851644f50f5eec6fe
Takuya Yoshioka <88071178+yoshioka1128@users.noreply.github.com>
2026-01-06 06:29:15 +0000

    renamed:    run_covariance.sh -> shell/run_covariance.sh 	renamed:    run_random_instances.sh -> shell/run_random_instances.sh

--- shell/run_covariance.sh
+++ shell/run_covariance.sh
@@ -6,5 +6,5 @@ hours=$(seq 1 24)
 # 各時間帯で Python スクリプトを実行
 for hour in $hours; do
     echo "時間帯 $hour の共分散行列を計算中..."
-    echo "$hour" | python covariance_matrix_mixup_restricted.py
+    echo "$hour" | python covariance_matrix_mixup_restricted_npzout.py
 done
